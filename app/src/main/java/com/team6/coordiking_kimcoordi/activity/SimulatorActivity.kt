ef2b47714a32c8e5715efe32f2d95dc78d7e7d66
dew <pjj3412@gmail.com>
2021-09-30 01:07:42 +0900

    commit

--- app/src/main/java/com/team6/coordiking_kimcoordi/activity/SimulatorActivity.kt
+++ app/src/main/java/com/team6/coordiking_kimcoordi/activity/SimulatorActivity.kt
@@ -2,6 +2,7 @@ package com.team6.coordiking_kimcoordi.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.team6.coordiking_kimcoordi.R
 import kotlinx.android.synthetic.main.activity_simulator.*
 
@@ -10,6 +11,22 @@ class SimulatorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simulator)
         setUpActionBar()
+
+        btn_Jacket.setOnClickListener {
+            iv_Jacket.setImageResource(R.drawable.image4)
+            Toast.makeText(this@SimulatorActivity, "사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        btn_Top.setOnClickListener {
+            iv_Top.setImageResource(R.drawable.image3)
+            Toast.makeText(this@SimulatorActivity, "사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        btn_Bottom.setOnClickListener {
+            iv_Bottom.setImageResource(R.drawable.image1)
+            Toast.makeText(this@SimulatorActivity, "사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+
     }
     private fun setUpActionBar(){
         setSupportActionBar(tb_simulator)
